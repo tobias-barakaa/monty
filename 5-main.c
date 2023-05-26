a5871b6a4f3eed7ceda5e44f651e38a679ac977b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-05-26 05:08:21 +0300

    Update 5-main.c

--- 5-main.c
+++ 5-main.c
@@ -1,25 +1,21 @@
 #include "monty.h"
-/**
- * pstr - print string
- * @stack: Addres of first element of stack
- * @line_number: Num of line
- */
+
 void pstr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = (*stack);
+	stack_t *elem = (*stack);
 
 	(void)line_number;
 	if (!stack || !(*stack))
 		goto exi;
 
-	while (tmp)
+	while (elem)
 	{
-		if (tmp->n == 0)
+		if (elem->n == 0)
 			goto exi;
-		if (tmp->n < 32 || tmp->n > 126)
+		if (elem->n < 32 || elem->n > 126)
 			goto exi;
-		printf("%c", tmp->n);
-		tmp = tmp->next;
+		printf("%c", elem->n);
+		elem = elem->next;
 	}
 exi: printf("\n");
 }
