53b734d55f4c2721556b428b4ca574e1b4ecc8f8
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-05-26 04:29:23 +0300

    Update 3-main.c

--- 3-main.c
+++ 3-main.c
@@ -1,31 +1,24 @@
 #include "monty.h"
 
-/**
- * sub - substract the top by the second top.
- * @stack: Head of the double linked list.
- * @line_number: Line of execution of command.
- * Return: Nothing, couse the struct specifications.
- */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	int res, i;
-	stack_t *tmp = *stack;
+	int status, iterate;
+	stack_t *elem = *stack;
 
-	for (i = 0; tmp != NULL; i++)
-		tmp = tmp->next;
-	if (i < 2 || *stack == NULL)
+	for (iterate = 0; elem != NULL; iterate++)
+		elem = elem->next;
+	if (iterate < 2 || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	tmp = (*stack)->next;
-	res = tmp->n - (*stack)->n;
+	elem = (*stack)->next;
+	status = elem->n - (*stack)->n;
 	free(*stack);
-	*stack = tmp;
-	(*stack)->n = res;
+	*stack = elem;
+	(*stack)->n = status;
 }
-
 
 /**
  * _div - divide the top by the second top.
