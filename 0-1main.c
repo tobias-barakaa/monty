54461d0b489bb26a50379dba38a67e3b09e4aef7
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-05-26 04:07:35 +0300

    Update 0-1main.c

--- 0-1main.c
+++ 0-1main.c
@@ -2,26 +2,26 @@
 
 void find_file(char *source, stack_t **stack)
 {
-	FILE *file;
+	FILE *open_file;
 	char *elem;
 	size_t n;
 	unsigned int line_number = 1;
 	char *buffer = NULL;
 
-	if (!source)
+	if (!source) 
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", source);
+		fprintf(stderr, "Error: Can't open  %s\n", source);
 		exit(EXIT_FAILURE);
 	}
 
-	file = fopen(source, "r");
-	if (!file)
+	open_file = fopen(source, "r");
+	if (!open_file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", source);
+		fprintf(stderr, "Error: Can't open open_file %s\n", source);
 		exit(EXIT_FAILURE);
 	}
-	header.file = file;
-	while (getline(&buffer, &n, file) != -1)
+	header.open_file = open_file;
+	while (getline(&buffer, &n, open_file) != -1)
 	{
 		header.buffer = buffer;
 		elem = strtok(buffer, " \n\t\r");
